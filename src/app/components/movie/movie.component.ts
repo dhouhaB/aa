7784a38c840636460c39799621cdc04879fb0802
dhouhaB <dhouha.bessalah@outlook.com>
2022-10-31 17:15:21 +0100

    first comt

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  type = '';
+  id = '';
+  url = '';
+  movies: any;
+  movie: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    this.type = this.route.snapshot.params['type'];
+    this.id = this.route.snapshot.params['id'];
+    this.getMovie()  }
+
+    getMovie() {
+      this.http.get(this.url).subscribe((movies) => {
+        this.movies = movies;
+        let index = this.movies.findIndex(
+          (movie: { id: string }) => movie.id == this.id
+        );
+        if (index > -1) {
+          this.movie = this.movies[index];
+        }
+      });
+    }
+   
 
 }
